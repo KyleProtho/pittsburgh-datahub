6a9697e39bbef7d053141c77d600380a106530a8
Kyle Protho <Onenonlykpro@gmail.com>
2020-03-30 16:14:54 -0400

    Update FetchCOVID19Data.py

--- data/FetchCOVID19Data.py
+++ data/FetchCOVID19Data.py
@@ -11,21 +11,32 @@ from os import path, mkdir
 # Import dataframe of case counts from Allegheny County Department of Health
 def FetchCOVID19Data():
     print("Retrieving COVID-19 case data from the Allegheny County Department of Health...")
-    item = GIS().content.get("e7fc66943d6f43bcba0c5d0294681d8d")
+    item = GIS(verify_cert = False).content.get("a0a47e3bff754268b2c4316fff8cbb6b")
     flayer = item.layers[0]
     df_covid19_case_counts = pd.DataFrame.spatial.from_layer(flayer)
     df_covid19_case_counts = pd.DataFrame(df_covid19_case_counts)
     
     
     # Convert from spatial dataframe to normal dataframe
-    df_covid19_case_counts = df_covid19_case_counts[["LABEL", "Numberofcases"]]
+    df_covid19_case_counts = df_covid19_case_counts[["LABEL", "Join_Count"]]
     
+    
+    # Rename county subdivision name and count columns
+    df_covid19_case_counts = df_covid19_case_counts.rename(
+        columns = {"LABEL": "NAME",
+                   "Join_Count": "Numberofcases"}
+    )
     
     # Add last updated date
     print("Adding updated date...")
     today_date = date.today()
     df_covid19_case_counts["UpdatedDate"] = today_date
     df_covid19_case_counts["UpdatedDate"] = pd.to_datetime(df_covid19_case_counts["UpdatedDate"])
+    
+    
+    # Add county and state columns
+    df_covid19_case_counts["COUNTY"] = "Allegheny"
+    df_covid19_case_counts["STATE"] = "Pennsylvania"
     
     
     # Export as .csv
